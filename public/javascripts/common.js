742354b5daebd680848409535685f82cb9fa07b2
Matt Tarbit <mtarbit@gmail.com>
2012-06-16 13:03:55 +0100

    Split things up into separate objects.

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,28 +1,63 @@
-var feed = {};
 
-feed.CHROMELESS_PLAYER_URL = "http://www.youtube.com/apiplayer?enablejsapi=1&version=3";
+// Feed - list of video thumbs
+
+var feed = {};
 
 feed.init = function(){
   this.container = $('.feed');
   this.items = this.container.find('.feed-item');
   this.items.on('click', this.itemClickHandler).css({ cursor: 'pointer' });
-  this.initPlayer();
+};
+
+feed.itemClickHandler = function(e){
+  var id = $(this).data('video-id');
+  if (id) player.play(id);
+  e.preventDefault();
+};
+
+// Player - chromeless YouTube swf
+
+var player = {};
+
+player.CHROMELESS_PLAYER_URL = "http://www.youtube.com/apiplayer?enablejsapi=1&version=3";
+
+player.init = function(){
+  this.initCallbacks();
+  this.initDom();
   this.initKeyboard();
 };
 
-feed.initPlayer = function(){
-  this.playerId = 'feed-player';
-  this.playerDivId = this.playerId + '-container';
+player.initDom = function(){
+  var innerId = this.domId = 'feed-player';
+  var outerId = innerId + '-container';
 
-  this.playerDiv = $('<div>').attr({ id: this.playerDivId, class: this.playerDivId }).text("You need Flash player 8+ to view this video.");
-  $('body').append(this.playerDiv);
+  this.container = $('<div>').attr({ id: outerId, class: outerId }).text("You need Flash player 8+ to view this video.");
+  $('body').append(this.container);
 
   var parameters = { allowScriptAccess: 'always' };
-  var attributes = { id: this.playerId };
-  swfobject.embedSWF(feed.CHROMELESS_PLAYER_URL, this.playerDivId, '100%', '100%', '8', null, null, parameters, attributes);
+  var attributes = { id: innerId };
+  swfobject.embedSWF(this.CHROMELESS_PLAYER_URL, outerId, '100%', '100%', '8', null, null, parameters, attributes);
 };
 
-feed.initKeyboard = function(){
+player.initCallbacks = function(){
+  var self = this;
+
+  window.onYouTubePlayerReady = function() {
+    self.getDom();
+  }
+
+  window.onYouTubePlayerStateChange = function(state) {
+    if (state == self.api.STATES.finished) self.stop();
+  }
+};
+
+player.getDom = function(){
+  this.dom = $('#' + this.domId);
+  this.dom.get(0).addEventListener('onStateChange', 'onYouTubePlayerStateChange');
+  this.api.init();
+}
+
+player.initKeyboard = function(){
   var keys = {
       37: 'lt'
     , 39: 'rt'
@@ -38,60 +73,56 @@ feed.initKeyboard = function(){
     var caught = true;
 
     switch (key) {
-
       case 'lt':    break;
       case 'rt':    break;
       case 'up':    break;
       case 'dn':    break;
-      case 'esc':   feed.stop();  break;
-      case 'space': feed.pause(); break;
+      case 'esc':   player.stop();  break;
+      case 'space': player.pause(); break;
       case 'enter': break;
 
       default:
         caught = false;
         break;
-
     }
 
     if (caught) e.preventDefault();
   });
 }
 
-feed.play = function(id){
-  this.player.css({ left: 0 });
-  this.player.get(0).loadVideoById(id);
+player.play = function(id){
+  this.dom.css({ left: 0 });
+  this.api.load(id);
 };
 
-feed.pause = function(){
-  var p = this.player.get(0);
-  if (p.getPlayerState() == 2) {
-    p.playVideo();
+player.pause = function(){
+  if (this.api.is('paused')) {
+    this.api.play();
   } else {
-    p.pauseVideo();
+    this.api.pause();
   }
 };
 
-feed.stop = function(){
-  this.player.css({ left: '-100%' });
-  this.player.get(0).pauseVideo();
+player.stop = function(){
+  this.dom.css({ left: '-100%' });
+  this.api.pause();
 };
 
-feed.itemClickHandler = function(e){
-  var id = $(this).data('video-id');
-  if (id) feed.play(id);
-  e.preventDefault();
-};
+var api = player.api = {};
+
+api.STATES = { 'finished':0, 'paused':2 };
+
+api.init = function(){ this.dom = player.dom.get(0); }
+api.load = function(id){ this.dom.loadVideoById(id); }
+api.play = function(){ this.dom.playVideo(); }
+api.pause = function(){ this.dom.pauseVideo(); }
+
+api.is = function(state){
+  return this.dom.getPlayerState() == this.STATES[state];
+}
 
 $(function(){
   feed.init();
+  player.init();
 });
-
-function onYouTubePlayerReady() {
-  feed.player = $('#' + feed.playerId);
-  feed.player.get(0).addEventListener('onStateChange', 'onYouTubePlayerStateChange');
-}
-
-function onYouTubePlayerStateChange(state) {
-  if (state == 0) feed.stop();
-}
 
